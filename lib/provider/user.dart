ea78d59fe09212b1c3ca45ea100fc5648502c17c
ricardodesouza1 <ricardo270296souza@gmail.com>
2021-10-28 08:52:31 -0300

    creating functions add, update, remove, crud data

--- lib/provider/user.dart
+++ lib/provider/user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/data/dummy_user.dart';
 import 'package:flutter_firebase/models/user.dart';
@@ -15,5 +17,40 @@ class Users with ChangeNotifier{
 
   User byIndex(int i){
     return _items.values.elementAt(i);
+  }
+
+  void put(User user) {
+    if(user == null){
+      return;
+    }
+    if(user.id != null && 
+       !user.id.trim().isNotEmpty && 
+       _items.containsKey(user.id)){
+
+      _items.update(user.id, (_) => User(
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          avatarUrl: user.avatarUrl,
+        ),
+      );
+    }else{
+      final id = Random().nextDouble().toString();
+      _items.putIfAbsent(id, () => User(
+        id: id,
+        name: user.name,
+        email: user.email,
+        avatarUrl: user.avatarUrl,
+      ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void remove(User user){
+    if(user != null && user.id != null){
+      _items.remove(user.id);
+      notifyListeners();
+    }
   }
 }
